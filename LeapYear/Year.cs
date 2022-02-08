bfc4d63d54579c7ce8b0a0cb380945ac02e70388
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-08 10:50:52 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ LeapYear/Year.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeapYearTask
+{
+    public static class Year
+    {
+        /// <summary>
+        /// Report if given year is a leap year.
+        /// </summary>
+        /// <param name="year">Given year.</param>
+        /// <returns>True if given year is leap, false otherwise.</returns>
+        public static bool IsLeapYear(int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
